c70daf9400ee311282d4bb797b098bcc6524cb02
Paul Statezny <paulstatezny@gmail.com>
2021-03-01 11:10:23 -0700

    Test back button with push_patch vs push_redirect

--- lib/back_button_web/live/page_live.ex
+++ lib/back_button_web/live/page_live.ex
@@ -3,37 +3,30 @@ defmodule BackButtonWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, number: 1)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("increment", _, socket) do
+    {:noreply, assign(socket, number: socket.assigns.number + 1)}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+  def handle_event("decrement", _, socket) do
+    {:noreply, assign(socket, number: socket.assigns.number - 1)}
   end
 
-  defp search(query) do
-    if not BackButtonWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  @impl true
+  def handle_event("push-patch", _, socket) do
+    {:noreply, push_patch(socket, to: "/#{socket.assigns.number}")}
+  end
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  @impl true
+  def handle_event("push-redirect", _, socket) do
+    {:noreply, push_redirect(socket, to: "/#{socket.assigns.number}")}
+  end
+
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
   end
 end
--- lib/back_button_web/router.ex
+++ lib/back_button_web/router.ex
@@ -17,7 +17,7 @@ defmodule BackButtonWeb.Router do
   scope "/", BackButtonWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    live "/:path_param", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
